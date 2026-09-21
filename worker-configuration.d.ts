b383e007ca2540d0ea35ba9787430d1ff5ed4940
READTalk <redtalk.app@hotmail.com>
2026-09-22 03:58:28 +0700

    Update mainModule import path to build directory

--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -7,7 +7,7 @@ interface __BaseEnv_Env {
 }
 declare namespace Cloudflare {
 	interface GlobalProps {
-		mainModule: typeof import("./src/index");
+		mainModule: typeof import("./build/server/index");
 	}
 	interface Env extends __BaseEnv_Env {}
 }
